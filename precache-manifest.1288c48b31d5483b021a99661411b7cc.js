9da666ef7539fd20024d91c0969ac5ce4d183567
riyandharne-123 <rockydods123@gmail.com>
2020-06-05 20:00:54 +0530

    Updates

---
+++ precache-manifest.1288c48b31d5483b021a99661411b7cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8971f83623427c7dfe615e3dc22e7f97",
+    "url": "/fun-food-friends/index.html"
+  },
+  {
+    "revision": "02f57ae7e428eaea38ed",
+    "url": "/fun-food-friends/static/css/main.a5853c71.chunk.css"
+  },
+  {
+    "revision": "a1a8038b4c053f62facd",
+    "url": "/fun-food-friends/static/js/2.e6e4e19b.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/fun-food-friends/static/js/2.e6e4e19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f57ae7e428eaea38ed",
+    "url": "/fun-food-friends/static/js/main.637119ca.chunk.js"
+  },
+  {
+    "revision": "8ceebb8fa2968105fcad",
+    "url": "/fun-food-friends/static/js/runtime-main.9a246f31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fun-food-friends/static/media/logo.5d5d9eef.svg"
+  }
+]);
